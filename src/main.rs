f2bcc16ab93dac540618832985b660caad3a5242
vxoxv <lilivxoxvilil@gmail.com>
2026-09-12 02:02:15 +0800

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,39 @@
-#[allow(unused_imports)]
+use std::collections::HashMap;
 use std::io::{self, Write};
 
+#[derive(Clone, Copy)]
+enum CmdType {
+    Builtin,
+}
+
 fn main() {
+    let type_map = HashMap::from([
+        ("echo", CmdType::Builtin),
+        ("exit", CmdType::Builtin),
+        ("type", CmdType::Builtin),
+    ]);
+
     loop {
         print!("$ ");
         io::stdout().flush().unwrap();
 
-        let mut cmd = String::new();
-        io::stdin().read_line(&mut cmd).unwrap();
-        cmd = cmd.trim().to_string();
+        let mut input = String::new();
+        io::stdin().read_line(&mut input).unwrap();
+        let args: Vec<_> = input.split_whitespace().collect();
 
-        match cmd.trim().split(' ').collect::<Vec<_>>().as_slice() {
-            [""] | [] => {}
+        match args.as_slice() {
+            [] => {}
             ["exit"] => break,
-            ["echo", rest @ ..] => println!("{}", rest.join(" ")),
-            [cmd, ..] => println!("{}: command not found", cmd),
+            ["echo", rest @ ..] => {
+                println!("{}", rest.join(" "));
+            }
+            ["type", cmd] => match type_map.get(cmd) {
+                Some(CmdType::Builtin) => println!("{} is a shell builtin", cmd),
+                None => println!("{}: not found", cmd),
+            },
+            [cmd, ..] => {
+                println!("{}: command not found", cmd);
+            }
         }
     }
 }
